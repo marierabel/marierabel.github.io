30495c6fe03339356df38e5d6983fbd5adf8128b
marierabel <157117670+marierabel@users.noreply.github.com>
2024-02-20 22:22:19 +0100

    Add files via upload

--- animt1.js
+++ animt1.js
@@ -1,3 +1,10 @@
+//animation scroll
+let titrintro = document.querySelector("#titreintro");
+
+function animation() {
+  titrintro.classList.add('animation');
+}
+
 //carrousselle 1
 const state = {};
 const carouselList = document.querySelector('.carousel__list');
@@ -11,19 +18,19 @@ carouselList.addEventListener('click', function (event) {
   if (!isItem || newActive.classList.contains('carousel__item_active')) {
     return;
   };
-  
+
   update(newActive);
 });
 
-const update = function(newActive) {
+const update = function (newActive) {
   const newActivePos = newActive.dataset.pos;
 
   const current = elems.find((elem) => elem.dataset.pos == 0);
   const prev = elems.find((elem) => elem.dataset.pos == -1);
   const next = elems.find((elem) => elem.dataset.pos == 1);
-  
+
   current.classList.remove('carousel__item_active');
-  
+
   [current, prev, next].forEach(item => {
     var itemPos = item.dataset.pos;
 
@@ -41,4 +48,28 @@ const getPos = function (current, active) {
   return diff;
 }
 
+//ham button
+
+let butham = document.querySelector("#ham");
+let hamenu = document.querySelector("#menuh");
+let iconco = document.querySelectorAll(".h");
+let h1 = document.querySelector("#h1")
+let h2 = document.querySelector("#h2")
+let h3 = document.querySelector("#h3")
+
+function togglemenu() {
+  if (hamenu.style.display === 'none') {
+    hamenu.style.display = 'block';
+    h1.classList.add('a');
+    h2.classList.add('b');
+    h3.classList.add('c');
+  } else {
+    hamenu.style.display = 'none';
+    h1.classList.remove('a');
+    h2.classList.remove('b');
+    h3.classList.remove('c');
+  }
+}
+
+butham.onclick = function () { togglemenu() };
 
--- main.js
+++ main.js
@@ -1,24 +1,69 @@
-var theme1=document.querySelector("#t1");
-theme1.style.transformOrigin="left top";
-theme1.onmouseenter = function(e){
-    console.log(e);
-    theme1.classList.add('expanded');
-    theme1.children[0].children[0].classList.add('cirexp');
-    theme1.children[0].classList.add('anipla');
-    toggleLayer("tiges");
+let theme1 = document.querySelector("#t1");
+let theme2 = document.querySelector("#t2");
+let theme3 = document.querySelector("#t3");
+let choix = document.querySelector("#choix");
+let imgt1 = document.querySelector("#imgpla");
+let cirt1 = document.querySelector("#imgcir");
+let t2cs = document.querySelector("#comsoon");
+let li1 = document.querySelector(".l1");
+let li2 = document.querySelector(".l2");
+let li3 = document.querySelector(".l3");
+let la1 = document.querySelector(".langages1");
+let la2 = document.querySelector(".langages2");
+let la3 = document.querySelector(".langages3");
+
+//theme1.style.transformOrigin = "left top";
+//theme2.style.transformOrigin = "bottom right";
+
+theme1.onmouseenter = function (e) {
+  console.log(e);
+  theme1.classList.add('expandedt1');
+  cirt1.classList.add('cirexp');
+  imgt1.classList.add('anipla');
+  li1.style.display = 'block';
+  la1.style.display = 'block';
+  //toggleLayer("tiges");
 };
-theme1.onmouseleave = function() {
-    theme1.classList.remove('expanded');
-    theme1.children[0].children[0].classList.remove('cirexp');
-    theme1.children[0].classList.remove('anipla');
-    toggleLayer("tiges");
+
+theme1.onmouseleave = function () {
+  theme1.classList.remove('expandedt1');
+  cirt1.classList.remove('cirexp');
+  imgt1.classList.remove('anipla');
+  li1.style.display = 'none';
+  la1.style.display = 'none';
+  //toggleLayer("tiges");
 }
 
+theme2.onmouseenter = function () {
+  theme2.classList.add('expandedt2');
+  li2.style.display = 'block';
+  la2.style.display = 'block';
+}
+
+theme2.onmouseleave = function () {
+  theme2.classList.remove('expandedt2');
+  li2.style.display = 'none';
+  la2.style.display = 'none';
+}
+
+theme3.onmouseenter = function () {
+  theme3.classList.add('expandedt3');
+  li3.style.display = 'block';
+  la3.style.display = 'block';
+}
+
+theme3.onmouseleave = function () {
+  theme3.classList.remove('expandedt3');
+  li3.style.display = 'none';
+  la3.style.display = 'none';
+}
+
+
 function toggleLayer(layerId) {
-    var layer = document.getElementById(layerId);
-    if (layer.style.display === 'none') {
-      layer.style.display = 'block';
-    } else {
-      layer.style.display = 'none';
-    }
+  let layer = document.getElementById(layerId);
+  if (layer.style.display === 'none') {
+    layer.style.display = 'block';
+  } else {
+    layer.style.display = 'none';
   }
+}
